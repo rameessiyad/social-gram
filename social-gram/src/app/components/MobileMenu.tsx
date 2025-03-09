4500a15fc1bb5609e4f3cfa5efdb6dc5d34578fb
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-03-09 23:13:17 +0530

    navbar styles completed

--- social-gram/src/app/components/MobileMenu.tsx
+++ social-gram/src/app/components/MobileMenu.tsx
@@ -5,33 +5,35 @@ import React, { useState } from "react";
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div onClick={() => setIsOpen((prev) => !prev)}>
-      <div className="flex flex-col gap-[4.5px] cursor-pointer">
-        <div
-          className={`w-6 h-1 bg-blue-500 rounded-sm origin-left ease-out duration-500 ${
-            isOpen ? "rotate-45" : ""
-          }`}
-        />
-        <div
-          className={`w-6 h-1 bg-blue-500 rounded-sm ease-out duration-500 ${
-            isOpen ? "opacity-0" : ""
-          }`}
-        />
-        <div
-          className={`w-6 h-1 bg-blue-500 rounded-sm origin-left ease-out duration-500 ${
-            isOpen ? "-rotate-45" : ""
-          }`}
-        />
-      </div>
-      {isOpen && (
-        <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl">
-          <Link href="/">Home</Link>
-          <Link href="/">Friends</Link>
-          <Link href="/">Groups</Link>
-          <Link href="/">Stories</Link>
-          <Link href="/">Login</Link>
+    <div className="md:hidden">
+      <div onClick={() => setIsOpen((prev) => !prev)}>
+        <div className="flex flex-col gap-[4.5px] cursor-pointer">
+          <div
+            className={`w-6 h-1 bg-blue-500 rounded-sm origin-left ease-out duration-500 ${
+              isOpen ? "rotate-45" : ""
+            }`}
+          />
+          <div
+            className={`w-6 h-1 bg-blue-500 rounded-sm ease-out duration-500 ${
+              isOpen ? "opacity-0" : ""
+            }`}
+          />
+          <div
+            className={`w-6 h-1 bg-blue-500 rounded-sm origin-left ease-out duration-500 ${
+              isOpen ? "-rotate-45" : ""
+            }`}
+          />
         </div>
-      )}
+        {isOpen && (
+          <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl">
+            <Link href="/">Home</Link>
+            <Link href="/friends">Friends</Link>
+            <Link href="/groups">Groups</Link>
+            <Link href="/stories">Stories</Link>
+            <Link href="/login">Login</Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- social-gram/src/app/components/Navbar.tsx
+++ social-gram/src/app/components/Navbar.tsx
@@ -1,20 +1,37 @@
 import Link from "next/link";
 import React from "react";
 import MobileMenu from "./MobileMenu";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
       {/* LEFT */}
-      <div className="">
+      <div className="md:hidden lg:block w-[20%]">
         <Link href="/" className="font-bold text-xl text-blue-600">
           SOCIALGRAM
         </Link>
       </div>
       {/* CENTER */}
-      <div className="hidden"></div>
+      <div className="hidden md:flex gap-4 w-[50%] text-sm">
+        {/* LINKS */}
+        <div className="flex gap-4 text-gray-600">
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/home.png" alt="home" width={16} height={16} />
+            <span>Homepage</span>
+          </Link>
+          <Link href="/friends" className="flex items-center gap-2">
+            <Image src="/friends.png" alt="home" width={16} height={16} />
+            <span>Friends</span>
+          </Link>
+          <Link href="/stories" className="flex items-center gap-2">
+            <Image src="/stories.png" alt="home" width={16} height={16} />
+            <span>Stories</span>
+          </Link>
+        </div>
+      </div>
       {/* RIGHT */}
-      <div className="">
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
         <MobileMenu />
       </div>
     </div>
