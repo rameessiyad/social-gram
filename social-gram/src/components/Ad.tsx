2169fd2ddfe8cd51be13dfe07fa912e2530fe5dc
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-03-10 11:13:48 +0530

    firend request ui created

---
+++ social-gram/src/components/Ad.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ad = ({ size }: { size: "sm" | "md" | "lg" }) => {
+  return <div className="p-2 bg-white rounded-lg shadow-md text-sm">Ad</div>;
+};
+
+export default Ad;
