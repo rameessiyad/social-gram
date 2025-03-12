37d4c15101b68cb481458fd611bbd3f14731076b
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-03-12 19:51:25 +0530

    user profile details fetched

--- social-gram/src/components/ProfileCard.tsx
+++ social-gram/src/components/ProfileCard.tsx
@@ -1,7 +1,28 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 import React from "react";
 
-const ProfileCard = () => {
+const ProfileCard = async () => {
+  const { userId } = await auth();
+
+  if (!userId) return null;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+        },
+      },
+    },
+  });
+
+  if (!user) return null;
+
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6 mb-6">
       <div className="h-20 relative">
@@ -12,7 +33,7 @@ const ProfileCard = () => {
           className="rounded-md object-cover"
         />
         <Image
-          src="https://images.pexels.com/photos/1413412/pexels-photo-1413412.jpeg?auto=compress&cs=tinysrgb&w=600"
+          src={user?.avatar || "/noAvatar.png"}
           alt=""
           width={48}
           height={48}
@@ -20,7 +41,11 @@ const ProfileCard = () => {
         />
       </div>
       <div className="h-20 flex flex-col gap-2 items-center">
-        <span className="font-semibold">Ramees Siyad</span>
+        <span className="font-semibold">
+          {user.name && user.surname
+            ? user.name + " " + user.surname
+            : user.username}
+        </span>
         <div className="flex items-center gap-4">
           <div className="flex">
             <Image
@@ -45,7 +70,9 @@ const ProfileCard = () => {
               className="rounded-full w-3 h-3 object-cover"
             />
           </div>
-          <span className="text-xs text-gray-500">500 followers</span>
+          <span className="text-xs text-gray-500">
+            {user._count.followers} followers
+          </span>
         </div>
         <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
           My Profile
