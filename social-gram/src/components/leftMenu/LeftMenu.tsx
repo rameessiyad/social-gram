e0125d97d57fbb6b362d22b96469501238a4001c
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-03-13 15:55:18 +0530

    import errors fixed

--- social-gram/src/components/leftMenu/LeftMenu.tsx
+++ social-gram/src/components/leftMenu/LeftMenu.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ProfileCard from "./ProfileCard";
 import Link from "next/link";
 import Image from "next/image";
-import Ad from "./Ad";
+import ProfileCard from "./ProfileCard";
+import Ad from "../Ad";
 
 const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
   return (
